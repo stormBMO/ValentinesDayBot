7d8980df4d6bce845b4a1c893efa4468d3ff668e
stormBMO <andrey.chet76@gmail.com>
2021-02-12 19:43:50 +0300

    Finnished work

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 # coding=utf-8
+import shutil
+import os
 import telebot
-
+from telebot.apihelper import ApiException
 # ----------------------------------------------------------------------------
 token_bot = '1654475418:AAHZ8MOBnHgH2t66qL4jvSbsrUqrFymufF8'
 bot = telebot.TeleBot(token_bot)
 flag_for_schedule = 0
 keyboard_hider = telebot.types.ReplyKeyboardRemove()
 to_who = ""
-
-# ----------------------------------------------------------------------------
-
-
+channel_id = "@pt_demid"
 # -----------------------------Text-------------------------------------------
+
+def is_subscribed(channel_id, user_id):
+    status = ['creator', 'administrator', 'member']
+    user_status = str(bot.get_chat_member(channel_id, user_id).status)
+    if user_status in status:
+        return True
+    else:
+        return False
+
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
-    if message.text == "/start":
-        markup = generate_start_markup()
-        bot.send_message(message.chat.id,
-                         "Привет! Это бот для валентинок (не знаю, придумайте сюда текст - я вставлю). Чтобы отправить валентинку, нажми кнопку ниже",
-                         reply_markup=markup)
+    if not is_subscribed(channel_id, message.from_user.id):
+        bot.send_message(message.chat.id, "Для использования бота подпишись на @pt_demid, а затем опять напиши /start")
+    else:
+        if message.text == "/start":
+            markup = generate_start_markup()
+            bot.send_message(message.chat.id,
+                             "Привет! Это бот для валентинок (не знаю, придумайте сюда текст - я вставлю). Чтобы отправить валентинку, нажми кнопку ниже",
+                             reply_markup=markup)
 
-    elif message.text == "Отправить валентинку":
-        bot.send_message(message.chat.id, "Сначала отпаравь мне кому ты хочешь отправить валентинку (Можешь инстаграмм, например), затем отправь саму валентинку",
-                         reply_markup=keyboard_hider)
-        bot.register_next_step_handler(message, get_valentine)
+        elif message.text == "Отправить валентинку":
+            bot.send_message(message.chat.id, "Сначала отпаравь мне кому ты хочешь отправить валентинку (Можешь инстаграмм, например), затем отправь саму валентинку",
+                             reply_markup=keyboard_hider)
+            bot.register_next_step_handler(message, get_valentine)
 
 
 #------------------------------Main logic--------------------------------------
@@ -42,14 +53,28 @@ def get_photo_messages(message):
     if to_who == "":
         bot.send_message(message.chat.id, "Для начала пройди все пункты до этого!")
     else:
+        to_who = to_who + "0"
+        markup = generate_start_markup()
         fileID = message.photo[-1].file_id
         file_info = bot.get_file(fileID)
-        print('file.file_path =', file_info.file_path)
         downloaded_file = bot.download_file(file_info.file_path)
-        with open(to_who + ".jpg", 'wb') as new_file:
+        photo_name = to_who + ".jpg"
+        backup_name = to_who[:-1]
+        backup_path_name = photo_name
+        with open(photo_name, 'wb') as new_file:
             new_file.write(downloaded_file)
-
-        bot.send_message(message.chat.id, "Супер, " + to_who + " получит твою валентинку.")
+        i = 1
+        while True:
+            if os.path.exists("photo/" + photo_name):
+                to_who = to_who[:-1]
+                to_who = to_who + str(i)
+                i = i + 1
+                photo_name = to_who + ".jpg"
+            else:
+                os.rename(backup_path_name, photo_name)
+                shutil.move(photo_name, "photo")
+                break
+        bot.send_message(message.chat.id, "Супер, " + backup_name + " получит твою валентинку.", reply_markup=markup)
         to_who = ""
 
 # -----------------------------keyboards----------------------------------------
